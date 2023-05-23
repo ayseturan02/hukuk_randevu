ee995cf9f37fdd5f2f293bd98291923f6d4529d2
AYŞE TURAN <ayse0025turan@gmail.com>
2023-05-23 22:38:08 +0300

    Update web.php

--- laravel_proje/routes/web.php
+++ laravel_proje/routes/web.php
@@ -13,15 +13,60 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/randevu', [App\Http\Controllers\RandevuController::class, 'index2'])->name('randevu');
 Route::get('/e-tahsilat', [App\Http\Controllers\E_tahsilatController::class, 'index3'])->name('e-tahsilat');
+
+
+Route::get('/', function () {
+    return view('welcome');
+})->name("welcome");
+
+Route::get('/giris', function () {
+    return view('giris');
+})->name("giris");
+
+Route::get('/kayıt', function () {
+    return view('kayıt');
+})->name("kayıt");
+
+Route::get('/hizmetlerimiz', function () {
+    return view("hizmetlerimiz");
+})->name("hizmetlerimiz");
+
+Route::get('/iletisim', function () {
+    return view("iletisim");
+})->name("iletisim");
+
+Route::get('/randevu', function () {
+    return view("randevu");
+})->name("randevu");
+
+Route::fallback(function (){
+   return view("hata");
+});
+
+Route::prefix("randevu")->group(function (){
+   Route::get("/kadro", function() {
+       return view("kadro");
+   });
+    Route::get("/hizmet", function() {
+        return view("hizmet");
+    });
+    Route::get("/takvim", function() {
+        return view("takvim");
+    });
+    Route::get("/saat", function() {
+        return view("saat");
+    });
+    Route::get("/onay", function() {
+        return view("onay");
+    });
+});
+
 
 
 
